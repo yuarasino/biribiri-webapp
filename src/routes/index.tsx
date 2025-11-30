221b39900d13385f8fe62a9854e021205fb05f45
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-11-30 13:57:49 +0900

    💄アイコンを追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Head } from "fresh/runtime";
+import { Icon } from "~/components/Icon.tsx";
 import { define } from "~/utils/typing.ts";
 import { cn } from "~/utils/styling.ts";
 
@@ -10,6 +11,12 @@ export default define.page(() => {
       </Head>
       <section>
         <h1 class={cn("text-pink-500")}>biribiri-webapp</h1>
+        <p class={cn("text-slate-500")}>
+          <Icon
+            icon="icon-[material-symbols-light--bolt]"
+            label="ビリビリ"
+          />
+        </p>
       </section>
     </main>
   );
