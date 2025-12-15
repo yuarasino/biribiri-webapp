de0a703c1e0e3166b326a8279dbd7242e175bfff
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2025-12-15 16:02:48 +0900

    🔧 ビルド生成物の出力先を変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import * as path from "@std/path";
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   root: "./src",
+  build: {
+    outDir: path.join(Deno.cwd(), "_fresh"),
+    emptyOutDir: true,
+  },
   plugins: [
     fresh(),
     tailwindcss(),
